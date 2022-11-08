0e4bea876ed9049081482d236dffaf5f4a804822
Tappobill <christian.salvati@hotmail.it>
2022-11-08 18:53:29 +0100

    Secondo Snack

--- jscript/main.js
+++ jscript/main.js
@@ -59,4 +59,68 @@ function svuotaFun() {
 
 
 
+let students = [
+    {
+        name: 'Marco',
+        id: 213,
+        grades: 78
+    },
+
+    {
+        name: 'Paola',
+        id: 110,
+        grades: 96
+    },
+
+    {
+        name: 'Andrea',
+        id: 250,
+        grades: 48
+    },
+
+    {
+        name: 'Gaia',
+        id: 145,
+        grades: 74
+    },
+
+    {
+        name: 'Luigi',
+        id: 196,
+        grades: 68
+    },
+
+    {
+        name: 'Piero',
+        id: 102,
+        grades: 50
+    },
+
+    {
+        name: 'Francesca',
+        id: 120,
+        grades: 84
+    },
+];
+
+let arrUppercase = students.map((element) => {
+    return element.name.toUpperCase();
+});
+
+console.log(arrUppercase);
+
+let arrGrades1 = students.filter((element) => {
+    if (element.grades > 70) {
+        return true;
+    }
+});
+
+console.log(arrGrades1);
+
+let arrGrades2 = students.filter(({ grades, id }) => grades > 70 && id > 120)
+
+console.log(arrGrades2)
+
+
+
 
